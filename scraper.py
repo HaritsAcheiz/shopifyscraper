1e22cd7b2b61377e55d48594ac2768a825a36dcc
Muhammad Harits R <harits.muhammad.only@gmail.com>
2023-03-28 15:21:18 +0700

    success get detail urls

--- scraper.py
+++ scraper.py
@@ -24,13 +24,20 @@ class shopyfyScraper:
             urls.append(url)
         return urls
 
+    def detail_parser(self, html):
+        tree = HTMLParser(html)
+        Product.name = tree.css_first('html > body > div:nth-child(5) > div:nth-child(2) > main > div:nth-child(2) > section > div > h1').text()
+        return Product.name
 
 if __name__ == '__main__':
     scraper = shopyfyScraper()
-    urls = [f'https://www.80stees.com/a/search?q=chrismas&page={str(page)}' for page in range(1,3)]
+    urls = [f'https://www.80stees.com/a/search?q=chrismas&page={str(page)}' for page in range(1,2)]
     htmls = [scraper.fetch(url) for url in urls]
     detail_urls = []
     for html in htmls:
         detail_urls.extend(scraper.parser(html))
-    print(len(detail_urls))
+    detail_htmls = [scraper.fetch(url) for url in detail_urls]
+    data = [scraper.detail_parser(html) for html in htmls]
+    print(data)
+    print(len(data))
 
